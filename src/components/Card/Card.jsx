83fb9140e253384b79dd8d49f49b396628e3b3c9
DarkMatter-999 <darkmatter999official@gmail.com>
2023-03-19 14:00:51 +0530

    Added Movie detail cards

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { Link } from "react-router-dom";
+
+import "./Card.css";
+
+const Card = ({ movie }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1500);
+    }, []);
+
+    return (
+        <>
+            {isLoading ? (
+                <div className="Cards">
+                    <SkeletonTheme color="#202020" highlightColor="#444">
+                        <Skeleton height={300} duration={2} />
+                    </SkeletonTheme>
+                </div>
+            ) : (
+                <Link
+                    to={`/movie/${movie.id}`}
+                    style={{ textDecoration: "none", color: "white" }}
+                >
+                    <div className="Cards">
+                        <img
+                            className="CardsImg"
+                            src={`https://image.tmdb.org/t/p/original${
+                                movie ? movie.poster_path : ""
+                            }`}
+                        />
+                        <div className="cardsOverlay">
+                            <div className="cardTitle">
+                                {movie ? movie.original_title : ""}
+                            </div>
+                            <div className="cardRuntime">
+                                {movie ? movie.release_date : ""}
+                                <span className="cardRating">
+                                    {movie ? movie.vote_average : ""}
+                                    <i className="fas fa-star" />
+                                </span>
+                            </div>
+                            <div className="cardDesc">
+                                {movie
+                                    ? movie.overview.slice(0, 118) + "..."
+                                    : ""}
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            )}
+        </>
+    );
+};
+
+export default Card;
